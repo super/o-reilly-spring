a5cd68f3c3ad87a681d017f65b39419460df392e
super <kelten3000@gmail.com>
2024-05-06 10:01:30 -0400

    First commit. Spring boilerplate

---
+++ src/main/java/com/keltenfalez/oreillyspring/OReillySpringApplication.java
@@ -0,0 +1,13 @@
+package com.keltenfalez.oreillyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OReillySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OReillySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keltenfalez/oreillyspring/OReillySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keltenfalez.oreillyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OReillySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
